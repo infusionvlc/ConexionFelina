2918b3dcabb99c617bb3336671b6f0eaf3c9a383
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-05-23 21:27:53 +0200

    Implemented tests for associations

--- spec/models/colony_spec.rb
+++ spec/models/colony_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Colony, type: :model do
+RSpec. describe Colony, type: :model do
   it { should belong_to :location }
   it { should have_and_belong_to_many :users }
   it { should have_many :cats }
