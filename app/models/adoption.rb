b4b6ff426f55d4a6ad097dfe60f7783a146c097b
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-05-23 17:44:52 +0200

    Implemented location, adoption, colony

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -1,2 +1,4 @@
 class Adoption < ApplicationRecord
+  belongs_to :cat
+  belongs_to :user
 end
--- app/models/colony.rb
+++ app/models/colony.rb
@@ -1,2 +1,6 @@
 class Colony < ApplicationRecord
+  belongs_to :location
+  has_many :cats
+
+  has_many_and_belongs_to_many :users
 end
