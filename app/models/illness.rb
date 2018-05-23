36909193ecd38c810577f85e32175d3ed72b3a10
Bertu <agy_life@hotmail.com>
2018-05-23 17:44:25 +0200

    Association of: Illness, Suffering and Treatment #25

--- app/models/illness.rb
+++ app/models/illness.rb
@@ -1,3 +1,6 @@
 class Illness < ApplicationRecord
 	enum priority: [ :low, :medium, :high ] 
+
+	belongs_to :sufferings
+  has_many :cats, through: :sufferings
 end
--- app/models/treatment_entry.rb
+++ app/models/treatment_entry.rb
@@ -1,3 +1,5 @@
 class TreatmentEntry < ApplicationRecord
 	enum treatment: [ :surgery, :diet, :medicine, :hospitalized, :hosted ]
+
+	belongs_to :suffering
 end
