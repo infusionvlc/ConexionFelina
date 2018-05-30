b1fd7a5edf0462a464c6c7abed5103f2432e7a3b
surfer8137 <asangar96@gmail.com>
2018-05-30 23:43:02 +0200

    Added validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
 	has_many :cats, through: :adoptions
 	has_many :sponsors
 	has_many :cats, through: :sponsors
+
+	validates :username, :email, :role, :purrs, presence: true
+ 	validates :username, length: { maximum: 20 }
+	validates :bio, length: { maximum: 512 }
+        validates :purrs, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 	
 end
