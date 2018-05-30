8bedc3f6c0a84442b75f2842311a7ee8567e6ff9
surfer8137 <asangar96@gmail.com>
2018-05-30 23:52:06 +0200

    Added tests to validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe User, type: :model do
   it { should have_many :adoptions}
   it { should have_many :donations}
   it { should have_many :posessions}
+
+	context 'validations' do
+		it { should validate_presence_of(:username) }
+		it { should validate_presence_of(:email) }
+		it { should validate_presence_of(:role) }
+		it { should validate_presence_of(:purrs) }
+		it { should validate_numericality_of(:purrs).only_integer }
+		it { should validate_numericality_of(:purrs).is_greater_than_or_equal_to 0 }
+	end
 end
