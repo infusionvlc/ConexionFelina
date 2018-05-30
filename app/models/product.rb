06bba5d9ceb98834053e9ed6a5b29ae2763eff5c
diphendara <cristiancvacas@gmail.com>
2018-05-31 00:50:00 +0200

    Add validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
+  MAXIMUM_NAME_LENGTH = 20
+  MINIMUM_STOCK_PRICE_VALUE = 0
+
   has_many :posessions
+
+  validates :name, :stock, :price, presence: true
+  validates :name, length: { maximum: MAXIMUM_NAME_LENGTH }
+  validates :stock, :price, numericality: { only_integer: true, greater_than_or_equal_to: MINIMUM_STOCK_PRICE_VALUE }
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it{ should have_many :posessions }
+
+  	context 'validations' do
+  		it { should validate_presence_of(:name) }
+      it { should validate_length_of(:name).is_at_most(50) }
+      it { should validate_presence_of(:stock) }
+  		it { should validate_numericality_of(:stock).only_integer }
+      it { should validate_numericality_of(:stock).is_greater_than_or_equal_to 0 }
+      it { should validate_presence_of(:price) }
+      it { should validate_numericality_of(:price).only_integer }
+      it { should validate_numericality_of(:price).is_greater_than_or_equal_to 0 }
+    end
 end
