bf99bfaff800ffd2a67c747aa38d98cfe8fe9632
surfer8137 <asangar96@gmail.com>
2018-06-02 00:37:27 +0200

    Fixed error text and condition for turn validator

--- app/validators/turn_validator.rb
+++ app/validators/turn_validator.rb
@@ -13,8 +13,8 @@ class TurnValidator < ActiveModel::Validator
 
     dates_exists = !start_date.nil? && !end_date.nil?
 
-    if dates_exists && record.start_date >= record.end_date
-      record.errors[:end_date] << 'End date must be later the end date'
+    if dates_exists && record.start_date > record.end_date
+      record.errors[:start_date] << 'End date must be later than the start date'
     end
   end
 end
