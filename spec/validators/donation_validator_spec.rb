b13ae71382e0ad9f511951318a80c0aac1def0fb
Cristian Cañadas Vacas <cristiancvacas@gmail.com>
2018-06-13 17:15:15 +0200

    Add mock items

--- spec/validators/donation_validator_spec.rb
+++ spec/validators/donation_validator_spec.rb
@@ -1,10 +1,16 @@
 require "rails_helper"
 
+
 RSpec.describe DonationValidator do
+
+  def mock_relationships(donation)
+    donation.user = mock_model('User')
+  end
+
   context "given a donation with an empty field" do
     it "must be invalid" do
       invalid_donation = Donation.new(amount: nil, date: nil)
-      invalid_donation.user = User.new()
+      mock_relationships(invalid_donation)
 
       expect(invalid_donation).to_not be_valid
     end
@@ -13,7 +19,7 @@ RSpec.describe DonationValidator do
   context "given a donation with its date earlier than today" do
     it "must be valid" do
       valid_donation = Donation.new(amount: 1, date: Date.yesterday)
-      valid_donation.user = User.new()
+      mock_relationships(valid_donation)
 
       expect(valid_donation).to be_valid
     end
@@ -22,7 +28,7 @@ RSpec.describe DonationValidator do
   context "given a donation with its date equal to today" do
     it "must be valid" do
       valid_donation = Donation.new(amount: 1, date: Date.today)
-      valid_donation.user = User.new()
+      mock_relationships(valid_donation)
 
       expect(valid_donation).to be_valid
     end
@@ -31,7 +37,7 @@ RSpec.describe DonationValidator do
   context "given a donation with it date later than today" do
     it "must be invalid" do
       invalid_donation = Donation.new(amount: 1, date: Date.tomorrow)
-      invalid_donation.user = User.new()
+      mock_relationships(invalid_donation)
 
       expect(invalid_donation).to_not be_valid
     end
@@ -40,7 +46,7 @@ RSpec.describe DonationValidator do
   context "given a donation with a negative amount" do
     it "must be invalid" do
       invalid_donation = Donation.new(amount: -1, date: Date.today)
-      invalid_donation.user = User.new()
+      mock_relationships(invalid_donation)
 
       expect(invalid_donation).to_not be_valid
     end
@@ -49,7 +55,7 @@ RSpec.describe DonationValidator do
   context "given a donation with a zero amount" do
     it "must be invalid" do
       invalid_donation = Donation.new(amount: 0, date: Date.today)
-      invalid_donation.user = User.new()
+      mock_relationships(invalid_donation)
 
       expect(invalid_donation).to_not be_valid
     end
@@ -58,7 +64,7 @@ RSpec.describe DonationValidator do
   context "given a donation with a positive amount" do
     it "must be valid" do
       valid_donation = Donation.new(amount: 1, date: Date.today)
-      valid_donation.user = User.new()
+      mock_relationships(valid_donation)
 
       expect(valid_donation).to be_valid
     end
