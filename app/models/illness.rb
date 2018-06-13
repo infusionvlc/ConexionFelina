95c503de0e33f443e2441dfe5b7111b21492713c
Cristian Cañadas Vacas <cristiancvacas@gmail.com>
2018-06-13 19:01:08 +0200

    Added illness validators

--- app/models/illness.rb
+++ app/models/illness.rb
@@ -1,6 +1,13 @@
 class Illness < ApplicationRecord
+	MAXIMUM_DESCRIPTION_LENGTH = 512
+	
 	enum priority: [ :low, :medium, :high ] 
 
 	has_many :sufferings
-  has_many :cats, through: :sufferings
+	has_many :cats, through: :sufferings
+	
+	validates :name, :description, :priority, :treatment, presence: true
+  validates :name, length: {maximum: 20}
+  validates :description, length: {maximum: MAXIMUM_DESCRIPTION_LENGTH}
+
 end
