a2e12b2e9de4100c933d280d23d561a48bef1b03
Carmen <carmelesmbm@gmail.com>
2018-06-14 16:39:27 +0200

    maximum values and validation date,presence,notes

--- app/models/treatment_entry.rb
+++ app/models/treatment_entry.rb
@@ -1,5 +1,12 @@
 class TreatmentEntry < ApplicationRecord
+	MAXIMUM_CHARACTERS = 1024
+
 	enum treatment: [ :surgery, :diet, :medicine, :hospitalized, :hosted ]
 
 	belongs_to :suffering
+
+	validates :date, presence: true
+    validates :treatment,  presence: true
+    validates :notes, length: { maximum: MAXIMUM_CHARACTERS }
+    validates_with TreatmentEntryValidator
 end
