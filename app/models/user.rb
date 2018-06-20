03dbf2ce7430880a7607be640b9bd0e0f0be9f39
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-06-20 18:12:46 +0200

    Added devise and pundit to repo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+				 :recoverable, :rememberable, :trackable, :validatable
+
 	MAXIMUM_USERNAME_LENGTH = 20
 	MAXIMUM_BIO_LENGTH = 512
 	MINIMUM_PURR_VALUE = 0
