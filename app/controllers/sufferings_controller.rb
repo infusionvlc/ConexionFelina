d7d29ab41d86b63704dcca0d68389ef84d281a0b
diphendara <cristiancvacas@gmail.com>
2018-07-15 17:20:00 +0200

    Add simple new suffering, TODOs in new.html.erb

--- app/controllers/sufferings_controller.rb
+++ app/controllers/sufferings_controller.rb
@@ -5,10 +5,26 @@ class SufferingsController < ApplicationController
     authorize @suffering
   end
 
+  def new
+    @suffering = Suffering.new
+  end
+
+  def create
+    @suffering = Suffering.new(suffering_params)
+    @suffering.cat_id = params[:cat_id]
+    @suffering.illness_id = params[:illness_id]
+    @suffering.save
+    redirect_to(cats_path)
+  end
+
   private
 
   def user_not_autorized
     flash[:alert] = "You are not autorized to perform this action"
     redirect_to suffering_path
+  end
+
+  def suffering_params
+    params.require(:suffering).permit(:diagnosis_date, :notes, :chronic, :status, :cat_id, :illness_id)
   end
 end
