4d6baac4546a2732598427c1b6ae5e256edc7093
Josep Dols <jodoldar@users.noreply.github.com>
2018-08-09 20:39:18 +0200

    First commit to clean code

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,31 +1,32 @@
 class AssignmentsController < ApplicationController
-    def index
-        @assignments = Assignment.all        
-    end
+  def index
+    @assignments = Assignment.all
+  end
 
-    def new
-        @assignment = Assignment.new(colony_id: params[:colony_id])        
-    end
+  def new
+    @assignment = Assignment.new(colony_id: params[:colony_id])
+  end
 
-    def create
-        @assignment = Assignment.new(assignment_params)
-        @assignment.save
-        redirect_to(colony_path(@assignment.colony_id))
-    end
+  def create
+    @assignment = Assignment.new(assignment_params)
+    @assignment.save
+    redirect_to(colony_path(@assignment.colony_id))
+  end
 
-    def show
-        @assignment = Assignment.find(params[:id])
-    end
+  def show
+    @assignment = Assignment.find(params[:id])
+  end
 
-    def destroy
-        @assignment = Assignment.find(params[:id])
-        @assignment.destroy
-        redirect_to(colony_path(@assignment.colony_id))
-    end
+  def destroy
+    @assignment = Assignment.find(params[:id])
+    @assignment.destroy
+    redirect_to(colony_path(@assignment.colony_id))
+  end
 
-    private
+  private
 
     def assignment_params
         params.require(:assignment).permit(:colony_id, :task_id)
     end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,42 @@
 class TasksController < ApplicationController
-    def index
-        @tasks = Task.all
-    end
+  def index
+    @tasks = Task.all
+  end
 
-    def new
-        @task = Task.new
-    end
+  def new
+    @task = Task.new
+  end
 
-    def edit
-        @task = Task.find(params[:id])
-    end
+  def edit
+    @task = Task.find(params[:id])
+  end
 
-    def create
-        @task = Task.new(task_params)
-        @task.save
-        redirect_to(tasks_path)
-    end
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to(tasks_path)
+  end
 
-    def update
-        @task = Task.find(params[:id])
-        @task.update(task_params)
-        redirect_to(tasks_path)
-    end
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to(tasks_path)
+  end
 
-    def show
-        @task = Task.find(params[:id])
-    end
+  def show
+    @task = Task.find(params[:id])
+  end
 
-    def destroy
-        @task = Task.find(params[:id])
-        @task.destroy
-        redirect_to(tasks_path)
-    end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to(tasks_path)
+  end
 
-    private
+  private
 
-    def task_params
-        params.require(:task).permit(:name, :description)
-    end
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
 end
