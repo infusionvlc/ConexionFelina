00f447118596b4b8e7a9920f7f12fa5388148a2b
diphendara <cristiancvacas@gmail.com>
2018-08-10 12:45:53 +0200

    Clean code code climate warnings

--- app/controllers/treatment_entries_controller.rb
+++ app/controllers/treatment_entries_controller.rb
@@ -32,14 +32,14 @@ class TreatmentEntriesController < ApplicationController
     @treatment_entry = TreatmentEntry.find(params[:id])
     authorize @treatment_entry
     @treatment_entry.update(treatment_entry_params)
-    redirect_to treatment_entries_path(:suffering_id => @treatment_entry.suffering_id)
+    redirect_to treatment_entries_path(suffering_id: @treatment_entry.suffering_id)
   end
 
   def destroy
     @treatment_entry = TreatmentEntry.find(params[:id])
     authorize @treatment_entry
     @treatment_entry.destroy
-    redirect_to treatment_entries_path(:suffering_id => @treatment_entry.suffering_id)
+    redirect_to treatment_entries_path(suffering_id: @treatment_entry.suffering_id)
   end
 
   private
