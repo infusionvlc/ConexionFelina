c747dccd043c3bebecaa2fbbfb37667cedeea57b
diphendara <cristiancvacas@gmail.com>
2018-08-10 11:38:12 +0200

    Add treatment_entry controller and new action

---
+++ app/controllers/treatment_entries_controller.rb
@@ -0,0 +1,40 @@
+class TreatmentEntriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @treatment_entry = TreatmentEntry.new
+    authorize @treatment_entry
+  end
+
+  def create
+    @treatment_entry = TreatmentEntry.new(treatment_entry_params)
+    @treatment_entry.suffering_id = params[:suffering_id]
+    authorize @treatment_entry
+    @treatment_entry.save
+    redirect_to(suffering_path(params[:suffering_id]))
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_not_autorized
+    flash[:alert] = "You are not autorized to perform this action"
+    redirect_to treatment_entry_path
+  end
+
+  def treatment_entry_params
+    params.require(:treatment_entry).permit(:date, :treatment, :notes, :suffering_id)
+  end
+end
