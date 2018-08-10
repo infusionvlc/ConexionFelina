04e75242e74764eda3290110eed8713876808f26
diphendara <cristiancvacas@gmail.com>
2018-08-10 19:17:54 +0200

    Close #107 and index action of assigments of a Colony

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -6,9 +6,16 @@ class TurnsController < ApplicationController
   end
 
   def new
+    @turn = Turn.new
+    authorize @turn
   end
 
   def create
+    @turn = Turn.new(turn_params)
+    @turn.assignment_id = params[:assignment_id]
+    authorize @turn
+    @turn.save
+    redirect_to assignments_path(colony_id: @turn.assignment_id)
   end
 
   def edit
@@ -21,7 +28,7 @@ class TurnsController < ApplicationController
 
   def user_not_autorized
     flash[:alert] = 'You are not autorized to perform this action'
-    redirect_to turn_path
+    redirect_to turns_path
   end
 
   def turn_params
