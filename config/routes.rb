df9b9c77b723b6ac990ec9618f8c5d87317991ce
diphendara <cristiancvacas@gmail.com>
2018-08-11 15:03:47 +0200

    Make wizard assignment task to a user

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   resources :tasks
   resources :assignments
   resources :turns
+  resources :task_user_wizard do
+    collection do
+      get 'assign_task_to_user'
+      post 'assign_task_to_user_register'
+    end
+  end
 
   HighVoltage.configure do |config|
     config.home_page = "home"
