5ccd18a56f8a5c0dc90d53fde2a3c85f27517331
diphendara <cristiancvacas@gmail.com>
2018-08-11 15:12:20 +0200

    Clean code

--- app/controllers/task_user_wizard_controller.rb
+++ app/controllers/task_user_wizard_controller.rb
@@ -1,10 +1,8 @@
 class TaskUserWizardController < ApplicationController
-
   def assign_task_to_user
     @colonies = Colony.all
     @tasks = Task.all
     @users = User.all
-##CAMBIAR POLICIES
   end
 
   def assign_task_to_user_register
@@ -13,7 +11,7 @@ class TaskUserWizardController < ApplicationController
     assignment.task_id = params[:task_id]
     assignment.save 
 
-    user = User.find(params[:user_id] )
+    user = User.find(params[:user_id])
     puts user.username
 
     turn = Turn.new(turn_params)
@@ -23,7 +21,7 @@ class TaskUserWizardController < ApplicationController
     turns_users = TurnsUser.new
     turns_users.user_id = user.id
     turns_users.turn_id = turn.id
-    turns_users.save   
+    turns_users.save
 
     redirect_to(cats_path)
   end
