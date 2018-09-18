2752f5258a55667189a2176b608a379ec9851931
Josep <jodoldar@gmail.com>
2018-09-18 20:01:20 +0200

    Picture component finished
    Added css to the component

--- app/javascript/stories/ui/picture.stories.js
+++ app/javascript/stories/ui/picture.stories.js
@@ -8,5 +8,7 @@ import { Picture } from '../../packs/ui-bundle';
 
 
 storiesOf('Picture', module)
-  .add('Basic usage', () => <Picture source="http://www.nyalatours.com/blog/wp-content/uploads/2017/03/10544419_713059368765067_1478240416863347491_n.jpg" alt_text="Imagen de prueba"></Picture>)
+  .add('Small image', () => <Picture source="http://www.nyalatours.com/blog/wp-content/uploads/2017/03/10544419_713059368765067_1478240416863347491_n.jpg" alt_text="Imagen de prueba pequeña" size="s"></Picture>)
+  .add('Medium image', () => <Picture source="http://www.nyalatours.com/blog/wp-content/uploads/2017/03/10544419_713059368765067_1478240416863347491_n.jpg" alt_text="Imagen de prueba mediana" size="m"></Picture>)
+  .add('Big image', () => <Picture source="http://www.nyalatours.com/blog/wp-content/uploads/2017/03/10544419_713059368765067_1478240416863347491_n.jpg" alt_text="Imagen de prueba grande" size="b"></Picture>)
 ;
