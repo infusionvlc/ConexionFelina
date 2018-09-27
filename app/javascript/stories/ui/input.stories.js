41c7c206d063f9412371dd1ae65cc3a938814fd5
diphendara <cristiancvacas@gmail.com>
2018-09-27 19:37:56 +0200

    Fix input style

--- app/javascript/stories/ui/input.stories.js
+++ app/javascript/stories/ui/input.stories.js
@@ -6,10 +6,8 @@ import { linkTo } from '@storybook/addon-links';
 import { Input } from '../../packs/ui-bundle';
 
 storiesOf('Input', module)
-  .add('Button', () => <Input type="button" value="Click here" />)
-  .add('Text', () => <Input type="text" />)
-  .add('Input between 5 and 10', () => <Input type="number" min="5" max="10"/>)
-  .add('Input between 1 and 3', () => <Input type="range" min="1" max="3" name="points"/>)
-  .add('Password', () => <Input type="Password" />)
-  .add('Date', () => <Input type="date" value="2018-09-22" />)
+  .add('Text', () => <Input type="text"/>)
+  .add('Text with name', () => <Input type="text" name="Alfredo"/>)
+  .add('Text with default value', () => <Input type="text" value="Default value"/>)
+  .add('Password', () => <Input type="password" value="You're curious, aren't you?"/>)
 ; 
