24f7ad4491fba685d1120b15fe306f840523b5e8
diphendara <cristiancvacas@gmail.com>
2018-09-27 19:37:56 +0200

    Fix input style

--- app/javascript/bundles/UI/components/Input/Input.jsx
+++ app/javascript/bundles/UI/components/Input/Input.jsx
@@ -1,36 +1,20 @@
-/**
- * Input.jsx - Created by Cristian
- * 
- * First edit: 22th Sep. 2018 - Cristian
- */
-
 import PropTypes from 'prop-types';
 import React from 'react';
 import css from './Input.scss';
 import classnames from 'classnames';
 
-const Input = ({type, value, disabled, min, max, maxlength, name, required, src, className }) => {
-return <input type={type} value={value} disabled={disabled} 
-              required={required} min={min} max={max} 
-              name={name} src={src} maxlength={maxlength}
-              className={className} />
+const Input = ({type, name, value}) => {
+  return <input className="input" type={type} name={name} value={value}/>;
 };
 
 export default Input;
 
 Input.propTypes = {
-  type: PropTypes.number,
-  value: PropTypes.number,
-  disabled: PropTypes.bool,
-  required: PropTypes.bool,
-  min: PropTypes.number,
-  max: PropTypes.number,
-  name: PropTypes.string,
-  src: PropTypes.string,
-  maxlength: PropTypes.number,
-  className: PropTypes.string
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  name: PropTypes.string
 }
-// Specifies the default values for props:
-Input.defaultProps = {
 
+Input.defaultProps = {
+  type: "text"
 }; 
