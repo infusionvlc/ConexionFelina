a24fad9d533544dd2a6af052a8f67eea69e47289
diphendara <cristiancvacas@gmail.com>
2018-09-30 12:42:15 +0200

    Add alert_component

--- app/javascript/packs/ui-bundle.js
+++ app/javascript/packs/ui-bundle.js
@@ -1,9 +1,12 @@
 import ReactOnRails from 'react-on-rails';
 import Input from '../bundles/UI/components/Input/Input';
 import Picture from '../bundles/UI/components/Picture/Picture';
+import Alert from '../bundles/UI/components/Alert/Alert';
+
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   Input,
-  Picture
+  Picture,
+  Alert
 });
-export {Input, Picture};
+export {Input, Picture, Alert};
