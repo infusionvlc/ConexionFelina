7fd792d4fd2b89b3572e69d0bd6030d58758ae79
Iswan Jumat <iswan@thinkcube.com>
2018-10-01 22:44:24 +0530

    added button component with block button support

--- app/javascript/packs/ui-bundle.js
+++ app/javascript/packs/ui-bundle.js
@@ -3,12 +3,14 @@ import Input from '../bundles/UI/components/Input/Input';
 import Picture from '../bundles/UI/components/Picture/Picture';
 import Alert from '../bundles/UI/components/Alert/Alert';
 import Card from '../bundles/UI/components/Card/Card';
+import Button from "../bundles/UI/components/Button/Button"
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   Input,
   Picture,
   Alert,
+  Button,
   Card
 });
 export { Input, Picture, Alert, Card };
