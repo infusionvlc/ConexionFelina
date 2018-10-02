d645ee45bab62a5b67d4f37998a57646d0c54b34
Iswan Jumat <iswan@thinkcube.com>
2018-10-02 09:12:21 +0530

    resolve all double quote issue

--- app/javascript/bundles/UI/components/Button/Button.jsx
+++ app/javascript/bundles/UI/components/Button/Button.jsx
@@ -1,19 +1,19 @@
-import PropTypes from "prop-types";
-import React from "react";
-import "./Button.scss";
-import classnames from "classnames";
+import PropTypes from 'prop-types';
+import React from 'react';
+import './Button.scss';
+import classnames from 'classnames';
 
-var types = ["primary", "error", "warning", "success", "disabled"];
+var types = ['primary', 'error', 'warning', 'success', 'disabled'];
 
 const Button = ({ type, children, block, transparent, ...rest }) => {
-  const classes = classnames("button", {
-    "button-primary": type === "primary",
-    "button-success": type === "success",
-    "button-warning": type === "warning",
-    "button-error": type === "error",
-    "button-disabled": type === "disabled",
-    "button-block": block,
-    "button-transparent": transparent
+  const classes = classnames('button', {
+    'button-primary': type === 'primary',
+    'button-success': type === 'success',
+    'button-warning': type === 'warning',
+    'button-error': type === 'error',
+    'button-disabled': type === 'disabled',
+    'button-block': block,
+    'button-transparent': transparent
   });
 
   return (
@@ -31,6 +31,6 @@ Button.propTypes = {
 
 // Specifies the default values for props:
 Button.defaultProps = {
-  type: "primary",
-  children: "Button"
+  type: 'primary',
+  children: 'Button'
 };
