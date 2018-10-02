741594ad142cd486a0457c762a4491acae759002
Iswan Jumat <iswan@thinkcube.com>
2018-10-02 09:12:21 +0530

    resolve all double quote issue

--- app/javascript/packs/ui-bundle.js
+++ app/javascript/packs/ui-bundle.js
@@ -3,7 +3,7 @@ import Input from '../bundles/UI/components/Input/Input';
 import Picture from '../bundles/UI/components/Picture/Picture';
 import Alert from '../bundles/UI/components/Alert/Alert';
 import Card from '../bundles/UI/components/Card/Card';
-import Button from "../bundles/UI/components/Button/Button"
+import Button from '../bundles/UI/components/Button/Button';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
--- app/javascript/stories/ui/button.stories.js
+++ app/javascript/stories/ui/button.stories.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-import { Button } from "../../packs/ui-bundle";
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { Button } from '../../packs/ui-bundle';
 
-storiesOf("Button", module)
-  .add("Primary", () => <Button type="primary">Next</Button>)
-  .add("Success", () => <Button type="success">Next</Button>)
-  .add("Warning", () => <Button type="warning">Next</Button>)
-  .add("Error", () => <Button type="error">Next</Button>)
-  .add("Disabled", () => <Button type="disabled">Next</Button>)
-  .add("Block Button", () => (
+storiesOf('Button', module)
+  .add('Primary', () => <Button type="primary">Next</Button>)
+  .add('Success', () => <Button type="success">Next</Button>)
+  .add('Warning', () => <Button type="warning">Next</Button>)
+  .add('Error', () => <Button type="error">Next</Button>)
+  .add('Disabled', () => <Button type="disabled">Next</Button>)
+  .add('Block Button', () => (
     <Button type="primary" block>
       Yes
     </Button>
   ))
-  .add("Transparent Block Button", () => (
+  .add('Transparent Block Button', () => (
     <Button type="success" block transparent>
       Yes
     </Button>
