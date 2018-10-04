faa483f8b306840fc6e1b1d8370bcd04bd61ee86
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-10-04 20:10:57 +0200

    improved modal component

--- app/javascript/bundles/UI/components/Modal/Modal.jsx
+++ app/javascript/bundles/UI/components/Modal/Modal.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import css from './Modal.scss';
 
 const Modal = ({ text }) => {
   return <div className="modal">
-    <div className="modal modalInterior">
+    <div className="modal-inner">
       {text}
     </div>
   </div>
