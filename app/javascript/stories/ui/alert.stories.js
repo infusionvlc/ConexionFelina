2d1abc82afa72131a225b5c20112c95f4209af4d
diphendara <cristiancvacas@gmail.com>
2018-10-06 17:26:01 +0200

    Add simple alert tests

--- app/javascript/stories/ui/alert.stories.js
+++ app/javascript/stories/ui/alert.stories.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import { Input, Alert } from '../../packs/ui-bundle';
 
 storiesOf('Alert', module)
-  .add('Primary', () => <Alert type='primary' content="Standard alert"/>)
-  .add('Success', () => <Alert type='success' content="Success alert"/>)
-  .add('Warning', () => <Alert type='warning' content="Warning alert"/>)
-  .add('Error', () => <Alert type='error' content="Error alert"/>)
-  .add('Disabled', () => <Alert type='disabled' content="Disabled alert"/>)
-  ; 
+  .add('Primary', () => <Alert type='primary' children="Standard alert"/>)
+  .add('Success', () => <Alert type='success' children="Success alert"/>)
+  .add('Warning', () => <Alert type='warning' children="Warning alert"/>)
+  .add('Error', () => <Alert type='error' children="Error alert"/>)
+  .add('Disabled', () => <Alert type='disabled' children="Disabled alert"/>);
--- app/javascript/stories/ui/input.stories.js
+++ app/javascript/stories/ui/input.stories.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { Input } from '../../packs/ui-bundle';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import { Input } from '../../packs/ui-bundle'
 
 storiesOf('Input', module)
   .add('Text', () => <Input type="text"/>)
@@ -12,5 +12,5 @@ storiesOf('Input', module)
   .add('Text with name', () => <Input type="text" name="Alfredo"/>)
   .add('Text with default value', () => <Input type="text" value="Default value"/>)
   .add('Text disabled', () => <Input type="text" value="Default value" disabled/>)
-  .add('Password', () => <Input type="password" value="You're curious, aren't you?"/>)
-; 
+  .add('Password', () => <Input type="password" value="You're curious, aren't you?"/>);
+ 
