88e65e85841e6b67defea1c1c43f9492d1efff77
Josep <jodoldar@gmail.com>
2018-10-16 17:59:12 +0200

    Local rebase solved

---
+++ app/javascript/bundles/UI/components/TextArea/TextArea.jsx
@@ -0,0 +1,32 @@
+/**
+ * TextArea.jsx - Created by Josep Dols
+ * 
+ * First edit: 19th Sep. 2018 - Josep Dols
+ */
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import css from './TextArea.scss';
+import classnames from 'classnames';
+
+const TextArea = ({readonly, cols, rows, name, maxlength}) => {
+  let className = "textarea";
+  if (readonly)
+    className += "readonly";
+  return <textarea className={className} name={name} cols={cols} rows={rows} maxLength={maxlength} readOnly={readonly}/>;
+};
+
+export default TextArea;
+
+TextArea.propTypes = {
+  cols: PropTypes.number,
+  rows: PropTypes.number,
+  name: PropTypes.string,
+  maxlength: PropTypes.number,
+  readonly: PropTypes.bool,
+};
+
+// Specifies the default values for props:
+TextArea.defaultProps = {
+
+};
