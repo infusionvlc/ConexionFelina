fed649801d14fba52b127d413d8f4225273e79b3
Patrick Owens <1919554+powens@users.noreply.github.com>
2018-10-15 20:47:09 -0700

    Added Avatar

--- app/javascript/packs/ui-bundle.js
+++ app/javascript/packs/ui-bundle.js
@@ -5,6 +5,7 @@ import Alert from '../bundles/UI/components/Alert/Alert';
 import Card from '../bundles/UI/components/Card/Card';
 import Button from '../bundles/UI/components/Button/Button';
 import Switch from '../bundles/UI/components/Switch/Switch';
+import Avatar from '../bundles/UI/components/Avatar/Avatar';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
@@ -14,5 +15,6 @@ ReactOnRails.register({
   Button,
   Card,
   Switch,
+  Avatar,
 });
-export { Input, Picture, Alert, Button, Card, Switch };
+export { Input, Picture, Alert, Button, Card, Switch, Avatar };
