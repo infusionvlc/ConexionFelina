9be20bbdcd3428a2d9007aa1c33962ebd081de0e
Giuly Formigo <giulyn97@gmail.com>
2018-10-29 21:01:57 +0100

    added illness CRUD

---
+++ app/controllers/illnesses_controller.rb
@@ -0,0 +1,54 @@
+class IllnessesController < ApplicationController
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def index
+    @illnesses = Illness.all
+  end
+
+  def new
+    @illness = Illness.new
+    authorize @illness
+  end
+
+  def create
+    @illness = Illness.new(illness_params)
+    authorize @illness
+    @illness.save
+    redirect_to( illnesses_path )
+  end
+
+  def edit
+    @illness = Illness.find( params[ :id ])
+    authorize @illness
+  end
+
+  def update
+    @illness = Illness.find( params[ :id ])
+    authorize @illness
+    @illness.update( illness_params )
+    redirect_to (illness_path( params[ :id ] ))
+  end
+
+  def show
+    @illness = Illness.find( params[ :id ])
+  end
+
+  def destroy
+    @illness = Illness.find( params[ :id ])
+    authorize @illness
+    @illness.destroy
+    redirect_to( illnesses_path )
+  end
+
+  private
+
+    def illness_params
+      params.require(:illness).permit(:name, :description, :priority, :treatment)
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not autorized to perform this action"
+      redirect_to illnesses_path
+    end
+end
---
+++ app/policies/illness_policy.rb
@@ -0,0 +1,29 @@
+class IllnessPolicy < ApplicationPolicy
+  attr_reader :user, :illness
+
+  def initialize(user, illness)
+    @user = user
+    @illness = illness
+  end
+
+  def new?
+    !@user.nil? && (@user.role == 'volunteer' || @user.role == 'admin')
+  end
+
+  def create?
+    new?
+  end
+  
+  def edit?
+    new?
+  end
+
+  def update?
+    new?
+  end
+
+  def destroy?
+    new?
+  end
+
+end
