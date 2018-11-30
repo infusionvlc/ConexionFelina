50072012ac52a31cb3c9b499601ee2717dcda25c
Álex <aarnal@robotnik.es>
2018-11-30 20:47:09 +0100

    added Pagination component

---
+++ app/javascript/packs/ui-bundle.js
@@ -0,0 +1,29 @@
+import ReactOnRails from 'react-on-rails';
+import Input from '../bundles/UI/components/Input/Input';
+import Picture from '../bundles/UI/components/Picture/Picture';
+import Alert from '../bundles/UI/components/Alert/Alert';
+import Button from '../bundles/UI/components/Button/Button';
+import Card from '../bundles/UI/components/Card/Card';
+import Switch from '../bundles/UI/components/Switch/Switch';
+import Modal from '../bundles/UI/components/Modal/Modal';
+import TextArea from '../bundles/UI/components/TextArea/TextArea';
+import Avatar from '../bundles/UI/components/Avatar/Avatar';
+import ProgressBar from '../bundles/UI/components/ProgressBar/ProgressBar';
+import Sidebar from '../bundles/UI/components/Sidebar/Sidebar';
+import Pagination from '../bundles/UI/components/Pagination/Pagination';
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+    Input,
+    Picture,
+    Alert,
+    Button,
+    Card,
+    Switch,
+    Modal,
+    TextArea,
+    Avatar,
+    Sidebar,
+    ProgressBar,
+    Pagination
+});
