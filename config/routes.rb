4f23fd8ea478a239c0098757f1c4c8ba565b27f1
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-12-06 23:25:03 +0100

    Added internationalization for new components

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
   resources :cats do
     resources :build, controller: 'cats/build'
