dd73cc9d87c6225a527545ea4bd160fc61dcb201
Josep <jodoldar@gmail.com>
2018-12-07 13:52:32 +0100

    Mino fix in RB stories

--- app/javascript/stories/ui/radiobutton.stories.js
+++ app/javascript/stories/ui/radiobutton.stories.js
@@ -7,4 +7,4 @@ storiesOf('RadioButton', module)
   .add('Basic RadioButton', () =>
     <RadioButton value="Input1" label="Test1"/>)
   .add('Checked RadioButton', () => 
-    <RadioButton value="Input1" label="Test2" checked="true"/>);
+    <RadioButton value="Input1" label="Test2" checked={true}/>);
