ae506e80f2b6f6431e36e8caa5d80e00b689c093
Josep <jodoldar@gmail.com>
2018-12-07 13:14:02 +0100

    Added CSS to the RadioButtons

--- app/javascript/bundles/UI/components/RadioButton/RadioButton.jsx
+++ app/javascript/bundles/UI/components/RadioButton/RadioButton.jsx
@@ -34,6 +34,11 @@ class RadioButton extends React.Component {
     return (
       <span>
         <input type="radio" className="radiobutton" value={this.props.value} onChange={function(){}} onClick={this.toggleOnClick} checked={this.props.checked}></input>
+        <svg width="20px" height="20px" viewBox="0 0 20 20">
+          <circle cx="10" cy="10" r="9"></circle>
+          <path d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z" class="inner"></path>
+          <path d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z" class="outer"></path>
+        </svg>
         <label for="">{this.state.label}</label>
       </span>
     );
