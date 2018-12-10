2618c3fb187147b8e0da0ee399089fff3d9c7596
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-12-10 21:12:18 +0100

    added tests for validation

--- app/validators/suffering_validator.rb
+++ app/validators/suffering_validator.rb
@@ -3,5 +3,8 @@ class SufferingValidator < ActiveModel::Validator
     if !record.diagnosis_date.nil? && record.diagnosis_date > Date.today
       record.errors[:diagnosis_date] << "Diagnosis date must be in the past"
     end
+    if record.dischargement_date.nil? && record.status != 'healty'
+      record.errors[:dischargement_date] << 'needs to be filled in for a sick cat'
+    end
   end
 end
--- spec/models/suffering_spec.rb
+++ spec/models/suffering_spec.rb
@@ -4,4 +4,19 @@ RSpec.describe Suffering, type: :model do
   it {should have_many :treatment_entries}
   it {should belong_to :cat}
   it {should belong_to :illness}
+
+  context 'dischargement_date' do
+    before(:all) do
+      Rails.application.load_seed
+    end
+    it 'should be valid' do
+      cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: Date.today)
+      expect(cat).to be_valid
+    end
+
+    it 'should not be valid with an empty dischargement date and other than healthy' do
+      cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: nil)
+      expect(cat).to_not be_valid
+    end
+  end
 end
