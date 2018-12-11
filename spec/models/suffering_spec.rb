f700a48d35db7a583cfdc8e151a4f8c9e48c9e83
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-12-11 18:01:59 +0100

    improved spec structure

--- spec/models/suffering_spec.rb
+++ spec/models/suffering_spec.rb
@@ -5,18 +5,21 @@ RSpec.describe Suffering, type: :model do
   it {should belong_to :cat}
   it {should belong_to :illness}
 
-  context 'dischargement_date' do
+  context 'validations' do
     before(:all) do
       Rails.application.load_seed
     end
-    it 'should be valid' do
-      cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: Date.today)
-      expect(cat).to be_valid
+    context 'when a a suffering for a cat is created' do
+      it 'should be valid' do
+        cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: Date.today)
+        expect(cat).to be_valid
+      end
     end
-
-    it 'should not be valid with an empty dischargement date and other than healthy' do
-      cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: nil)
-      expect(cat).to_not be_valid
+    context 'when a suffering for a cat is created with a nil dischargement_date' do
+      it 'should not be valid with an empty dischargement date and other than healthy' do
+        cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: nil)
+        expect(cat).to_not be_valid
+      end
     end
   end
 end
