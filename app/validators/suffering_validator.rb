0f6b18af770833509462f6d7e7b525ef90744aff
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2018-12-12 20:24:19 +0100

    added more specs and fixed the other ones

--- app/validators/suffering_validator.rb
+++ app/validators/suffering_validator.rb
@@ -3,9 +3,9 @@ class SufferingValidator < ActiveModel::Validator
     if !record.diagnosis_date.nil? && record.diagnosis_date > Date.today
       record.errors.add(:diagnosis_date, 'Diagnosis date must be in the past')
     end
-    if record.dischargement_date.nil? && record.status != 'healty'
+    if record.status.eql?(4) && record.dischargement_date.nil?
       record.errors.add(:dischargement_date,
-                        'Needs to be filled in for a sick cat')
+                        'Needs to be filled in for a healthy cat')
     end
   end
 end
--- spec/models/suffering_spec.rb
+++ spec/models/suffering_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Suffering, type: :model do
+
   it {should have_many :treatment_entries}
   it {should belong_to :cat}
   it {should belong_to :illness}
@@ -9,15 +10,35 @@ RSpec.describe Suffering, type: :model do
     before(:all) do
       Rails.application.load_seed
     end
-    context 'when a a suffering for a cat is created' do
-      it 'should be valid' do
-        cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: Date.today)
+
+    context 'when a suffering for a healty cat is created' do
+      it 'should be valid with a status other than healthy and a dischargement date' do
+        cat = Cat.first.sufferings.build(diagnosis_date: Date.today, notes: '', chronic: false, status: 0, illness_id: 1, dischargement_date: Date.today)
         expect(cat).to be_valid
       end
-    end
-    context 'when a suffering for a cat is created with a nil dischargement_date' do
-      it 'should not be valid with an empty dischargement date and other than healthy' do
-        cat = Cat.first.sufferings.create(diagnosis_date: Date.today, notes: '', chronic: false, status: 'initial', illness_id: 1, dischargement_date: nil)
+
+      it 'should not be valid with a healthy status and an empty dischargement date' do
+        cat = Cat.first.sufferings.build(diagnosis_date: Date.today, notes: '', chronic: false, status: 4, illness_id: 1, dischargement_date: nil)
+        expect(cat).to_not be_valid
+      end
+
+      it 'should be valid with a healthy status and an dischargement date' do
+        cat = Cat.first.sufferings.build(diagnosis_date: Date.today, notes: '', chronic: false, status: 4, illness_id: 1, dischargement_date: Date.today)
+        expect(cat).to be_valid
+      end
+      
+      it 'should be valid with a status other than healthy and an empty dischargement date' do
+        cat = Cat.first.sufferings.build(diagnosis_date: Date.today, notes: '', chronic: false, status: 0, illness_id: 1, dischargement_date: nil)
+        expect(cat).to be_valid
+      end
+
+      it 'should not be valid with a nil status a a nil dischargement_date' do
+        cat = Cat.first.sufferings.build(diagnosis_date: Date.today, notes: '', chronic: false, status: nil, illness_id: 1, dischargement_date: nil)
+        expect(cat).to_not be_valid
+      end
+
+      it 'should not be valid with a nil status and a dischargement_date' do
+        cat = Cat.first.sufferings.build(diagnosis_date: Date.today, notes: '', chronic: false, status: nil, illness_id: 1, dischargement_date: Date.today)
         expect(cat).to_not be_valid
       end
     end
