19512d6fb71fa30c1aa5ceaa8ebc7cf29c31c1c8
Álvaro Casado Coscollá <alvarocasadoc@gmail.com>
2018-12-17 16:52:11 +0100

    Bumped colonies form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
+  get '/league', to: 'cats#league'
+
   devise_for :users,
     controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
 
+  devise_scope :user do
+    get '/join', to: 'users/registrations#new_volunteer'
+  end
+
   resources :cats do
     resources :build, controller: 'cats/build'
+
 
     member do
       get 'adopt'
