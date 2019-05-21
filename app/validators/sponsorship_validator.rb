6e3c59a573bdd1aec9772d0d0dd71620c89d94e8
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2019-05-21 12:27:41 +0200

    code climate issues

--- app/validators/sponsorship_validator.rb
+++ app/validators/sponsorship_validator.rb
@@ -3,8 +3,9 @@ class SponsorshipValidator < ActiveModel::Validator
 
   def validate(record)
     if !record.last_payment_date.nil? &&
-        record.last_payment_date > Date.today
-      record.errors[:last_payment_date] << 'Last payment date must be in the past'
+      record.last_payment_date > Date.today
+      record.errors[:last_payment_date] <<
+      'Last payment date must be in the past'
     end
   end
 end
