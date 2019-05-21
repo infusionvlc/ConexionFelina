a51913bd72a4966d0eac5673dd3a1208dac143b7
Manuel Lorenzo Parejo <manulorenzop@gmail.com>
2019-05-21 12:21:28 +0200

    more code style issues

--- app/validators/sponsorship_validator.rb
+++ app/validators/sponsorship_validator.rb
@@ -2,10 +2,9 @@ class SponsorshipValidator < ActiveModel::Validator
   MINIMUM_AMOUNT_VALUE = 0
 
   def validate(record)
-    if !record.last_payment_date.nil? && 
+    if !record.last_payment_date.nil? &&
         record.last_payment_date > Date.today
-      record.errors[:last_payment_date] << 
-      "Last payment date must be in the past"
+      record.errors[:last_payment_date] << 'Last payment date must be in the past'
     end
   end
 end
